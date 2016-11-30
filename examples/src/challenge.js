9305e7437b3c17ee814d1bc8571dc07dcd76a4d0
Mark Wales <mark@smallhadroncollider.com>
2016-11-30 15:06:24 +0000

    added first version to challenge.js

--- examples/src/challenge.js
+++ examples/src/challenge.js
@@ -1,5 +1,11 @@
 import fetchJSON from "./fetchJSON";
 
 fetchJSON("/coders.json").then(function (json) {
-    console.log(json);
+    var age = 0;
+
+    for (var i = 0, l = json.length; i < l; i++) {
+        age += json[i].age;
+    }
+
+    console.log(age);
 });
